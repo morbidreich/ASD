eb4c52ca8f501e2f44041ba954a024eceeb5c91d
Bartek Kujda <bartek.kujda@gmail.com>
2021-11-14 13:40:21 +0100

    extracted classes for drawing different types of fixes

--- src/main/java/io/github/morbidreich/ui/drawing/FixSymbolDrawer.java
+++ src/main/java/io/github/morbidreich/ui/drawing/FixSymbolDrawer.java
@@ -7,32 +7,31 @@ import java.awt.*;
 
 public class FixSymbolDrawer {
     public static void drawFixSymbol(int x, int y, Graphics g, Fix fix) {
-        //for vfr fixes symbol is circle
-        if (fix.getFixType() == FixType.VFR) {
-            g.drawOval(x, y, 5, 5);
-        } else if (fix.getFixType() == FixType.AERODROME) {
-            
-            int dim1 = 6;
-            int dim2 = 8;
-            
-            g.drawOval(x-dim1, y-dim1, dim1 * 2, dim1 * 2);
-            g.drawLine(x + dim1, y, x + dim2, y);
-            g.drawLine(x - dim1, y, x - dim2, y);
-            g.drawLine(x, y + dim1, x, y + dim2);
-            g.drawLine(x, y - dim1, x, y - dim2);
+        switch (fix.getFixType()) {
+            case VFR: g.drawOval(x, y, 5, 5); break;
+            case AERODROME: AerodromeSymbolDrawer.draw(x, y, g); break;
+            case SEARCH_RESULT: SearchResultSymbolDrawer.draw(g, x, y); break;
+            case VOR: VorSymbolDrawer.draw(g, x, y); break;
+            case DME: DmeSymbolDrawer.draw(g, x, y); break;
+            default: DefaultSymbolDrawer.draw(g, x, y); break;
         }
-        else if (fix.getFixType() == FixType.SEARCH_RESULT) {
-            int dim1 = 6;
-            g.drawOval(x-dim1, y-dim1, dim1 * 2, dim1 * 2);
-            g.drawLine(x - dim1, y, x + dim1, y);
-            g.drawLine(x, y+dim1, x, y-dim1);
-        }
-        //each other type's symbol is triangle
-        else {
-            g.drawLine(x, y - 4, x + 5, y + 3);
-            g.drawLine(x + 5, y + 3, x - 5, y + 3);
-            g.drawLine(x - 5, y + 3, x, y - 4);
-        }
+
+
+
+
+//        if (fix.getFixType() == FixType.VFR) {
+//
+//        }
+//        else if (fix.getFixType() == FixType.AERODROME) {
+//            AerodromeSymbolDrawer.draw(x, y, g);
+//        }
+//        else if (fix.getFixType() == FixType.SEARCH_RESULT) {
+//            SearchResultSymbolDrawer.draw(g, x, y);
+//        }
+//        //each other type's symbol is triangle
+//        else {
+//            DefaultSymbolDrawer.draw(g, x, y);
+//        }
         //consider adding fancy symbol for VORs, NDBs and think of one for cities
     }
 }
