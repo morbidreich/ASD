03b9dcab703a7b734207e0677029bd73f999f86f
Bartek Kujda <bartek.kujda@gmail.com>
2021-11-14 15:16:49 +0100

    updated switch statements in Colors

--- src/main/java/io/github/morbidreich/utils/Colors.java
+++ src/main/java/io/github/morbidreich/utils/Colors.java
@@ -27,6 +27,7 @@ public class Colors {
     public Color TSA_COLOR = new Color(250, 90, 0, 122);
     public Color TRA_COLOR = new Color(150, 150, 150, 122);
     public Color AREA_COLOR = new Color(150, 150, 150, 122);
+    public Color VORDME = new Color(80, 80, 80, 122);
     public Color SEARCH_RESULT_COLOR = new Color(250,90,0,255);
     public static Color TRACK_COLOR = new Color(0,255,0,255);
     public static Color TRACK_COLOR_SPI = new Color(0,190,255);
@@ -34,113 +35,72 @@ public class Colors {
 
 
     public Color getColor(PolygonType pt) {
-        switch (pt) {
-            case CTR: return CTR_COLOR;
-            case TMA: return TMA_COLOR;
-            case SID: return SID_COLOR;
-            case STAR: return STAR_COLOR;
-            case VFR: return VFR_COLOR;
-            case FIX: return FIX_COLOR;
-            case RIVER: return RIVER_COLOR;
-            case TOWN: return TOWN_COLOR;
-            case ROAD: return ROAD_COLOR;
-            case BORDER: return BORDER_COLOR;
-            case D: return D_COLOR;
-            case P: return P_COLOR;
-            case R: return R_COLOR;
-            case TRA: return TRA_COLOR;
-            case TSA: return TSA_COLOR;
-            case AREA: return AREA_COLOR;
-            case SEARCH_RESULT: return SEARCH_RESULT_COLOR;
-
-            default: return new Color(255,255,122);
-        }
+        return switch (pt) {
+            case CTR -> CTR_COLOR;
+            case TMA -> TMA_COLOR;
+            case SID -> SID_COLOR;
+            case STAR -> STAR_COLOR;
+            case VFR -> VFR_COLOR;
+            case FIX -> FIX_COLOR;
+            case RIVER -> RIVER_COLOR;
+            case TOWN -> TOWN_COLOR;
+            case ROAD -> ROAD_COLOR;
+            case BORDER -> BORDER_COLOR;
+            case D -> D_COLOR;
+            case P -> P_COLOR;
+            case R -> R_COLOR;
+            case TRA -> TRA_COLOR;
+            case TSA -> TSA_COLOR;
+            case AREA -> AREA_COLOR;
+            case SEARCH_RESULT -> SEARCH_RESULT_COLOR;
+            default -> new Color(255, 255, 122);
+        };
     }
 
     public void setColor(PolygonType pt, Color color) {
         switch (pt) {
-            case CTR -> {
-                CTR_COLOR = color;
-            }
-            case TMA -> {
-                TMA_COLOR = color;
-            }
-            case SID -> {
-                SID_COLOR = color;
-            }
-            case STAR -> {
-                STAR_COLOR = color;
-            }
-            case VFR -> {
-                VFR_COLOR = color;
-            }
-            case FIX -> {
-                FIX_COLOR = color;
-            }
-            case RIVER -> {
-                RIVER_COLOR = color;
-            }
-            case TOWN -> {
-                TOWN_COLOR = color;
-            }
-            case ROAD -> {
-                ROAD_COLOR = color;
-            }
-            case BORDER -> {
-                BORDER_COLOR = color;
-            }
-            case TSA -> {
-                TSA_COLOR = color;
-            }
-            case TRA -> {
-                TRA_COLOR = color;
-            }
-            case AREA -> {
-                AREA_COLOR = color;
-            }
-            case P -> {
-                P_COLOR = color;
-            }
-            case R -> {
-                R_COLOR = color;
-            }
-            case D -> {
-                D_COLOR = color;
-            }
-            case UNDEFINED -> {
-            }
+            case CTR -> CTR_COLOR = color;
+            case TMA -> TMA_COLOR = color;
+            case SID -> SID_COLOR = color;
+            case STAR -> STAR_COLOR = color;
+            case VFR -> VFR_COLOR = color;
+            case FIX -> FIX_COLOR = color;
+            case RIVER -> RIVER_COLOR = color;
+            case TOWN -> TOWN_COLOR = color;
+            case ROAD -> ROAD_COLOR = color;
+            case BORDER -> BORDER_COLOR = color;
+            case TSA -> TSA_COLOR = color;
+            case TRA -> TRA_COLOR = color;
+            case AREA -> AREA_COLOR = color;
+            case P -> P_COLOR = color;
+            case R -> R_COLOR = color;
+            case D -> D_COLOR = color;
+            case UNDEFINED -> {}
         }
     }
 
 
     public Color getColor(FixType ft) {
-        switch (ft) {
-            case SID01:
-            case SID19:
-            case DER:
-                return SID_COLOR;
-            case STAR01:
-            case STAR19:
-                return STAR_COLOR;
-            case ENTRY:
-            case VOR:
-            case DME:
-            case AERODROME:
-                return AERODROME_COLOR;
-            case ACC_FIX: return ACC_FIX_COLOR;
-            case VFR: return VFR_COLOR;
-            case TOWN: return TOWN_COLOR;
-            case SEARCH_RESULT: return SEARCH_RESULT_COLOR;
-            case UNDEFINED:
-            default: return new Color(255,255,122);
-        }
+        return switch (ft) {
+            case SID01, SID19, DER -> SID_COLOR;
+            case STAR01, STAR19 -> STAR_COLOR;
+            case VOR, DME -> VORDME;
+            case ENTRY, AERODROME -> AERODROME_COLOR;
+            case ACC_FIX -> ACC_FIX_COLOR;
+            case VFR -> VFR_COLOR;
+            case TOWN -> TOWN_COLOR;
+            case SEARCH_RESULT -> SEARCH_RESULT_COLOR;
+            case UNDEFINED -> new Color(255, 255, 122);
+            default -> new Color(255, 255, 123);
+        };
     }
     public void setColor(FixType fixType, Color color) {
         switch (fixType) {
             case SID01, SID19, DER -> SID_COLOR = color;
             case STAR01, STAR19 -> STAR_COLOR = color;
             case VFR -> VFR_COLOR = color;
-            case ENTRY, VOR, AERODROME -> AERODROME_COLOR = color;
+            case ENTRY, AERODROME -> AERODROME_COLOR = color;
+            case VOR, DME -> VORDME = color;
             case ACC_FIX -> ACC_FIX_COLOR = color;
             case TOWN -> TOWN_COLOR = color;
             case UNDEFINED -> {}
